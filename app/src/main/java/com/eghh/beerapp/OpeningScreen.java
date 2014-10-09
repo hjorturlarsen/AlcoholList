e710c710cf0f7ea579a9cb2f61dd158f40af4028
gudnithor4 <gudnithor4@gmail.com>
2014-10-09 12:54:00 +0000

    upphrópunarmerki

--- app/src/main/java/com/eghh/beerapp/OpeningScreen.java
+++ app/src/main/java/com/eghh/beerapp/OpeningScreen.java
@@ -37,6 +37,6 @@ public class OpeningScreen extends Activity {
         //Drullaðu upp í þig hóran þín
         //Larsen reporting in
         //second check
-        //Hrólfur er betri en þið allir faggarnir ykkar
+        //Hrólfur er betri en þið allir faggarnir ykkar!
     }
 }

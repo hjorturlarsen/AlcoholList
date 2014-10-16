90ec56749a545499835d3f6c9fc3da33b41da7ac
Hjörtur Larsen Þórðarson <91hjortur@gmail.com>
2014-10-16 11:56:12 +0000

    Basic search function

--- app/src/main/java/com/eghh/beerapp/common/fragments/search.java
+++ app/src/main/java/com/eghh/beerapp/common/fragments/search.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.widget.SearchView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class search extends Fragment{
+
+
     public static search newInstance()
     {
         return new search();
@@ -18,8 +22,30 @@ public class search extends Fragment{
     {
         super.onActivityCreated(savedInstanceState);
 
-        //getView().findViewById(R.id.fragment_search);
+        final View view = getView().findViewById(R.id.fragment_search);
+
+        //search window
+        final SearchView searchView = (SearchView) view.findViewById(R.id.search_search);
+        //expand by default
+        searchView.setIconifiedByDefault(false);
+        //listener
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                ((TextView)view.findViewById(R.id.search_textView)).setText(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
     }
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)

d850565f8280a301ecc9fe75fafd12eae32dd142
GilliHeiti <egill.ingolfsson@gmail.com>
2014-10-23 16:21:52 +0000

    Merge branch 'master' of C:\Users\egill\Documents\beerapp with conflicts.

--- app/src/main/java/com/eghh/beerapp/common/activities/SplashActivity.java
+++ app/src/main/java/com/eghh/beerapp/common/activities/SplashActivity.java
@@ -2,17 +2,23 @@ package com.eghh.beerapp.common.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.eghh.beerapp.common.fragments.R;
 
 public class SplashActivity extends Activity {
+    private static String TAG = SplashActivity.class.getName();
+    private static long SLEEP_TIME = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash_screen);
         super.onCreate(savedInstanceState);
 
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Removes title bar
@@ -20,37 +26,48 @@ public class SplashActivity extends Activity {
 
         setContentView(R.layout.activity_splash);
 
-        // Start background work
-        new DataBaseWorkWhileSplash().execute();
+        // Start timer and launch main activity
+        IntentLauncher launcher = new IntentLauncher();
+        launcher.start();
     }
 
-    private class DataBaseWorkWhileSplash extends AsyncTask<String, Void, Boolean> {
 
-        public DataBaseWorkWhileSplash(){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.splash, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
         }
+        return super.onOptionsItemSelected(item);
+    }
 
-        protected void onPreExecute() {
-            //Maybe nothing
-
-        }
-
+    private class IntentLauncher extends Thread {
         @Override
-        protected void onPostExecute(final Boolean success) {
+        /**
+         * Sleep for some time and than start new activity.
+         */
+        public void run() {
+            try {
+                // Sleeping
+                Thread.sleep(SLEEP_TIME*1000);
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+
             // Start main activity
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
             SplashActivity.this.startActivity(intent);
             SplashActivity.this.finish();
-        }
-
-        protected Boolean doInBackground(String... args) {
-            int x = 0;
-            while (x < 4000000){
-                x++;
-            }
-            //DataBase work will be done here
-
-            return null;
         }
     }
 }
